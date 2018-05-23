2f9357a0732b1e4ec3a8b5efb60ddf6f10d3c2fb
Ilya <ilyazuev133@gmail.com>
2018-05-23 13:17:14 +1000

    Add field classes for enums and numbers

--- include/fieldwidget.h
+++ include/fieldwidget.h
@@ -1,18 +1,55 @@
 #pragma once
 
 #include <QWidget>
+#include <QLineEdit>
 #include <QTextBrowser>
 #include <QListWidget>
+#include <QComboBox>
+
+#include "rapidjson/document.h"
 
 class FieldWidget : public QWidget {
     Q_OBJECT
 public:
-    FieldWidget(QString titleText, int valueText, QWidget *parent = 0);
+    FieldWidget(rapidjson::Value *field, QWidget *parent = 0);
     void setTitleText(QString text);
-    void setValueText(QString text);
-    void setValueText(int text);
+    virtual void setEnabledValueW(bool) = 0;
     QString getTitleText() const;
+    virtual void setField(rapidjson::Value *value) = 0;
+    virtual void update() = 0;
+protected:
+    void initTitleW();
+    void init();
+    QTextBrowser *titleW;
+    rapidjson::Value *field;
+};
+
+class NumbFW : public FieldWidget {
+    Q_OBJECT
+public:
+    NumbFW(rapidjson::Value *field, QWidget *parent = 0);
+    void setEnabledValueW(bool) override;
+    void setField(rapidjson::Value *value) override;
+    void update() override;
 private:
-    QTextBrowser *titleTb;
-    QTextBrowser *valueTb;
+    void checkCorrect(const QString &text);
+    void initValueW();
+    QLineEdit *valueW;
+    int min;
+    int max;
+private slots:
+    void slotTextChanged(const QString &text);
+};
+
+class EnumFW : public FieldWidget {
+    Q_OBJECT
+public:
+    EnumFW(rapidjson::Value *field, QWidget *parent = 0);
+    void setEnabledValueW(bool) override;
+    void setField(rapidjson::Value *value) override;
+    void update() override;
+private:
+    std::map<int, int> indeces;
+    void initValueW();
+    QComboBox *valueW;
 };
--- src/fieldwidget.cpp
+++ src/fieldwidget.cpp
@@ -1,24 +1,35 @@
 #include <QLayout>
+#include <cmath>
 
 #include "fieldwidget.h"
 
-FieldWidget::FieldWidget(QString titleText, int valueText, QWidget *parent) :
+using namespace rapidjson;
+
+FieldWidget::FieldWidget(rapidjson::Value *field, QWidget *parent) :
     QWidget(parent) {
-    titleTb = new QTextBrowser(this);
-    titleTb->setFixedWidth(80);
-    titleTb->setText(titleText);
-//    titleTb->setFrameStyle(QFrame::NoFrame);
+    this->field = field;
 
-    valueTb = new QTextBrowser(this);
-    valueTb->setText(QString::number(valueText));
-//    valueTb->setFrameStyle(QFrame::NoFrame);
+    initTitleW();
+//    initValueW();
+    init();
+}
 
-    setFixedHeight(30);
+void FieldWidget::initTitleW() {
+    titleW = new QTextBrowser(this);
+    titleW->setFixedWidth(100);
+    titleW->setText((*field)["title"].GetString());
 
+//    int height = std::ceil(titleW->toPlainText().size() / titleW->width()) * 30;
+//    titleW->setFixedHeight(height);
+    titleW->setFixedHeight(45);
+//    titleW->setEnabled(false);
+//    titleW->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Expanding);
+}
+
+void FieldWidget::init() {
     QHBoxLayout *hbl = new QHBoxLayout;
-    hbl->addWidget(titleTb);
-    hbl->addWidget(valueTb);
-//    hbl->addStretch();
+    hbl->addWidget(titleW);
+//    hbl->addWidget(valueW);
     hbl->setMargin(0);
     hbl->setContentsMargins(0, 0, 0, 0);
     hbl->setSpacing(0);
@@ -26,17 +37,93 @@ FieldWidget::FieldWidget(QString titleText, int valueText, QWidget *parent) :
 }
 
 void FieldWidget::setTitleText(QString text) {
-    titleTb->setText(text);
-}
-
-void FieldWidget::setValueText(QString text) {
-    valueTb->setText(text);
-}
-
-void FieldWidget::setValueText(int text) {
-    valueTb->setText(QString::number(text));
+    titleW->setText(text);
 }
 
 QString FieldWidget::getTitleText() const {
-    return titleTb->toPlainText();
+    return titleW->toPlainText();
+}
+
+NumbFW::NumbFW(rapidjson::Value *field, QWidget *parent) :
+    FieldWidget(field, parent) {
+    initValueW();
+    layout()->addWidget(valueW);
+}
+
+void NumbFW::initValueW() {
+    valueW = new QLineEdit(this);
+    min = (*field)["min"].GetInt();
+    max = (*field)["max"].GetInt();
+    connect(valueW, SIGNAL(textChanged(QString)), this, SLOT(slotTextChanged(QString)));
+    valueW->setText(QString::number((*field)["value"].GetInt()));
+    valueW->setEnabled(false);
+    valueW->setValidator(new QIntValidator(-999999, 999999, this));
+    valueW->setFixedHeight(45);
+}
+
+void NumbFW::slotTextChanged(const QString &text) {
+    checkCorrect(text);
+}
+
+void NumbFW::checkCorrect(const QString &text) {
+    if ((text.toInt() < min || text.toInt() > max) && min != max)
+        valueW->setStyleSheet("QLineEdit { background-color: red }");
+    else
+        valueW->setStyleSheet("QLineEdit { background-color: white }");
+}
+
+void NumbFW::setField(rapidjson::Value *value) {
+    field = value;
+    min = (*field)["min"].GetInt();
+    max = (*field)["max"].GetInt();
+    titleW->setText((*field)["title"].GetString());
+    valueW->setText(QString::number((*field)["value"].GetInt()));
+}
+
+void NumbFW::update() {
+    (*field)["value"].SetInt(valueW->text().toInt());
+}
+
+void NumbFW::setEnabledValueW(bool param) {
+    valueW->setEnabled(param);
+}
+
+EnumFW::EnumFW(rapidjson::Value *field, QWidget *parent) :
+    FieldWidget(field, parent) {
+    initValueW();
+    layout()->addWidget(valueW);
+}
+
+void EnumFW::initValueW() {
+    valueW = new QComboBox(this);
+    int index = 0;
+    for (auto &item: (*field)["items"].GetArray()) {
+        valueW->insertItem(index, item["title"].GetString(), item["value"].GetInt());
+        indeces.emplace(item["value"].GetInt(), index);
+        index++;
+    }
+//    valueW->setCurrentIndex(indeces.at((*field)["currentItem"].GetInt()));
+    valueW->setEnabled(false);
+}
+
+void EnumFW::setField(rapidjson::Value *value) {
+    field = value;
+    titleW->setText((*field)["title"].GetString());
+    int currentItem = (*field)["currentItem"].GetInt();
+    if (indeces.find(currentItem) != indeces.end()) {
+        valueW->setCurrentIndex(indeces.at(currentItem));
+        valueW->setStyleSheet("QComboBox { background-color: white }");
+    }
+    else {
+        valueW->setStyleSheet("QComboBox { background-color: red }");
+        valueW->setCurrentIndex(0);
+    }
+}
+
+void EnumFW::update() {
+    (*field)["currentItem"].SetInt(valueW->itemData(valueW->currentIndex()).toInt());
+}
+
+void EnumFW::setEnabledValueW(bool param) {
+    valueW->setEnabled(param);
 }
